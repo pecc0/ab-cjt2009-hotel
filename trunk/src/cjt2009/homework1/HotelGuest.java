8e8c7c7a649f70832a11e5fb1e77b010b371e7e2
air_biker@abv.bg <air_biker@abv.bg@d06871e0-3b4a-11de-8fef-1f5ae417a2df>
2009-05-10 16:56:36 +0000

    added javadoc

--- trunk/src/cjt2009/homework1/HotelGuest.java
+++ trunk/src/cjt2009/homework1/HotelGuest.java
@@ -1,5 +1,12 @@
 package cjt2009.homework1;
 
+/**
+ * Extends the base guest in order to add field for string the 
+ * room number that the guest has rented
+ * 
+ * @author Petko Petkov
+ *
+ */
 public class HotelGuest extends Guest {
 	private static final long serialVersionUID = -4638018475153420490L;
 	private int roomNumber = -1;
--- trunk/src/cjt2009/homework1/HotelRoom.java
+++ trunk/src/cjt2009/homework1/HotelRoom.java
@@ -2,26 +2,39 @@ package cjt2009.homework1;
 
 import java.util.ArrayList;
 
-
+/**
+ * Extends the base Room with a field where to store a list of e-mails 
+ * of the guests that are currently using this room
+ * Also added method to check whether the room is occupied
+ * 
+ * @author Petko Petkov
+ *
+ */
 public class HotelRoom extends Room {
 	private static final long serialVersionUID = -482459580656499567L;
 	private ArrayList<String> guestEmails = new ArrayList<String>();
 	
+	/**
+	 * Constructs a Hotel room from base room
+	 * @param r The base room
+	 */
 	public HotelRoom(Room r){
 		setCapacity(r.getCapacity());
 		setNumber(r.getNumber());
 		setRate(r.getRate());
 	}
-	
+	/**
+	 * Whether the room has guests in it
+	 * @return Whether the room has guests in it
+	 */
 	public boolean isOccupied(){
 		return !getGuestEmails().isEmpty();
 	}
 
-	public void setGuestEmails(ArrayList<String> guestEmails) {
-		
-		this.guestEmails = guestEmails;
-	}
-
+	/**
+	 * Returns the e-mails of the gusts in the room. Used to read and manipulate with these e-mails  
+	 * @return The e-mails of the gusts in the room 
+	 */
 	public ArrayList<String> getGuestEmails() {
 		return guestEmails;
 	}
